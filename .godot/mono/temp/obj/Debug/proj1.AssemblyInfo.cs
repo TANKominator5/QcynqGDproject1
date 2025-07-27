a139eafa2b122ff1a1c1664222dd5a5d5931f6db
tirthankarm2006 <tirthankarm2006@gmail.com>
2025-07-27 21:58:38 +0530

    added physics dinosaur/chnages the island a bit

--- .godot/mono/temp/obj/Debug/proj1.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/proj1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proj1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c736f36f62489594e45c9bed2856a5519ec742")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f93a8aaf6d64aee2d2febe63a11e921fb1afc7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("proj1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proj1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
